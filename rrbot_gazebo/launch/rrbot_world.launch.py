826a6f2cfda8ae8b8cac59182403b174e29909b9
ahcorde <ahcorde@gmail.com>
2021-02-03 10:59:27 +0100

    Ported rrbot_gazebo package
    Signed-off-by: ahcorde <ahcorde@gmail.com>

---
+++ rrbot_gazebo/launch/rrbot_world.launch.py
@@ -0,0 +1,101 @@
+# Copyright 2021 Open Robotics (2021)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+import xacro
+import yaml
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:
+        # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+    # moveit_cpp.yaml is passed by filename for now since it's node specific
+    rrbot_gazebo = os.path.join(
+        get_package_share_directory('rrbot_gazebo'),
+        'worlds',
+        'rrbot.world')
+
+    print(rrbot_gazebo)
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                launch_arguments={'world': rrbot_gazebo}.items(),
+             )
+
+    rrbot_description_path = os.path.join(
+        get_package_share_directory('rrbot_description'))
+
+    xacro_file = os.path.join(rrbot_description_path,
+                              'urdf',
+                              'rrbot.xacro')
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description_config = doc.toxml()
+    robot_description = {'robot_description': robot_description_config}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'rrbot'],
+                        output='screen')
+
+    # Static TF
+    static_tf = Node(package='tf2_ros',
+                     executable='static_transform_publisher',
+                     name='static_transform_publisher',
+                     output='log',
+                     arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'world', 'link1'])
+
+    return LaunchDescription([
+      gazebo,
+      node_robot_state_publisher,
+      static_tf,
+      spawn_entity
+    ])
